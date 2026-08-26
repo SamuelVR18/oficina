3e9c2a1c93cf865a4b90bc2413bd591fa6ae5feb
samuelvrocha <samuelvrocha03@gmail.com>
2026-08-26 18:04:18 -0300

    Testes

--- programa.cs
+++ programa.cs
@@ -1,7 +1,12 @@
 using System;
 
 class Program {
-    void main(){
+    static void Main(){
         Console.WriteLine("Teu Pai");
+        Console.WriteLine("Quantos Reias vc tem pra me dar");
+        int dinheiro = int.Parse(Console.ReadLine());
+        if(dinheiro == 0 ){
+            onsole.WriteLine("Deixa de ser mão de vaca");
+        }
     }
 }
